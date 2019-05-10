79a9a4e8cd93941ede61d102e3491df1b7527d41
Rob <ravenstudiosrob@yahoo.com>
2019-05-10 17:59:26 -0500

    P5 Template init commit

---
+++ game.js
@@ -0,0 +1,24 @@
+var gameWidth = 600;
+var gameHeight = 400;
+
+
+
+
+function setup(){
+	createCanvas(gameWidth, gameHeight);
+
+}
+
+
+function update(){
+
+
+}
+
+
+function draw(){
+
+	
+
+	update();
+}
